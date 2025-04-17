88e3b41aa28f8f2ff493fed97f9ef0cb24faed17
Sangeet-Chourasia <64009352+Sangeet-Chourasia@users.noreply.github.com>
2025-04-17 14:28:48 +0530

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -1,20 +1,14 @@
 <?php
-
 include 'app/functions.php';
 if (!file_exists($loginFilePath)) {http_response_code(401); echo 'Login required.'; exit;}
-header('Content-Type: audio/x-mpegurl');
-header('Content-Disposition: attachment; filename="playlist.m3u"');
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $origin_api);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
+header("Content-Type: application/json");
+//header('Content-Type: audio/x-mpegurl');
+//header('Content-Disposition: attachment; filename="playlist.m3u"');
+$filename = 'data.json';
+$response = @file_get_contents($filename);
 $data = json_decode($response, true);
-$channels = $data['data']['list'] ?? [];
+$channels = $data['data']['channels'] ?? [];
 if (!is_array($channels)) {http_response_code(500);echo "# Error: Invalid or missing 'list' in response\n";exit;}
-$skip_ids_json = @file_get_contents($stb_only);
-$skip_ids = json_decode($skip_ids_json, true);
-if (!is_array($skip_ids)) $skip_ids = [];
 if (stripos($userAgent, 'tivimate') !== false) {$liveheaders = '| X-Forwarded-For=59.178.74.184 | Origin=https://watch.tataplay.com | Referer=https://watch.tataplay.com/';} elseif  (stripos($userAgent, 'SparkleTV') !== false) {$liveheaders = '|X-Forwarded-For=59.178.74.184|Origin=https://watch.tataplay.com|Referer=https://watch.tataplay.com/';} else {$liveheaders = '|X-Forwarded-For=59.178.74.184&Origin=https://watch.tataplay.com&Referer=https://watch.tataplay.com/';}
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'];
@@ -29,17 +23,15 @@ if ($is_apache) {$htaccess_path = '.htaccess';
     $stream_path = file_exists($htaccess_path) ? "manifest.mpd" : "get-mpd.php";} else {
     $stream_path = "get-mpd.php";
 }
-
+echo "#EXTM3U x-tvg-url=https://avkb.short.gy/epg.xml.gz\n\n";
 foreach ($channels as $channel) {
-    $channel_id = $channel['id'];
-    if (in_array($channel_id, $skip_ids, true)) {continue;}
-    if (isset($channel['provider']) && $channel['provider'] === 'DistroTV') {continue;}
-    $channel_name = $channel['title'];
-    $channel_logo = $channel['transparentImageUrl'];
-    $channel_genre = $channel['genres'][0] ?? 'General';
-    if (in_array('HD', $channel['genres'])) {$channel_genre .= ", HD";}
-
+    $channel_id = $channel['id'];    
+    $channel_name = $channel['name'];
+    $channel_logo = $channel['logo_url'];
+    $channel_genre = $channel['primaryGenre'];
+        
     $license_url = "https://tp.drmlive-01.workers.dev?id={$channel_id}";
+    
     $channel_live = "{$base_url}/{$stream_path}?id={$channel_id}{$liveheaders}";
 
     echo "#EXTINF:-1 tvg-id=\"ts{$channel_id}\" tvg-logo=\"{$channel_logo}\" group-title=\"{$channel_genre}\",{$channel_name}\n";
